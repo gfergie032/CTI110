8189e99319e8502a965024f6b48589b07e66e50c
gfergie032 <44270490+gfergie032@users.noreply.github.com>
2018-11-08 11:49:44 -0500

    Add files via upload

---
+++ P2 Practice.py
@@ -0,0 +1,17 @@
+# CTI 110
+# P2 Practice
+# Gayon Ferguson
+# October 26, 2018
+
+# Ask user their age.
+currentAge = int(input("How old are you: " ))
+
+# Tell The user their next age.
+nextAge = currentAge+1
+
+# How old you will be next year.
+print("Next year you will be", nextAge )
+
+
+
+
